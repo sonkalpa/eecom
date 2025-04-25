2ea08528d14fff1b933c1edf6d24c14276bdb188
Sonkalpa Borah <sonkalpa123456@gmail.com>
2025-04-25 07:03:19 +0530

    chore: update config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/eecom',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true,
+    domains: ['images.pexels.com'],
   },
 };
 
