b198e01694f6bdec792c39efc24b5ea8dffe1682
Sonkalpa Borah <sonkalpa123456@gmail.com>
2025-04-25 06:57:36 +0530

    chore: add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/eecom',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
